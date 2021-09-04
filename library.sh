799859b30ed3e1ff8ac19de338117ac6da087846
Lucas Girouard-Stranks <lustranks@gmail.com>
2021-09-04 19:02:38 -0400

    Update build script for Windows

--- library.sh
+++ library.sh
@@ -1,59 +1,88 @@
 #!/bin/bash
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-uname_str="$(uname | tr '[:upper:]' '[:lower:]')"
-echo "Started building native libraries... OS: $uname_str, Directory: $script_dir"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+uname_str="$(uname -a)"
+case "${uname_str}" in
+    *Microsoft*)    OS="Microsoft";;
+    *microsoft*)    OS="Microsoft";;
+    Linux*)         OS="Linux";;
+    Darwin*)        OS="Apple";;
+    CYGWIN*)        OS="Linux";;
+    MINGW*)         OS="Microsoft";;
+    *Msys)          OS="Microsoft";;
+    *)              OS="UNKNOWN:${uname_str}"
+esac
+if [[ "$OS" == "Microsoft" ]]; then
+    CMAKE_TOOLCHAIN_ARGS="-DCMAKE_TOOLCHAIN_FILE=$DIR/mingw-w64-x86_64.cmake"
+elif [[ "$OS" == "Linux" ]]; then
+    CMAKE_TOOLCHAIN_ARGS=""
+elif [[ "$OS" == "Apple" ]]; then
+    CMAKE_TOOLCHAIN_ARGS=""
+fi
+
+echo "Started building native libraries... OS: $OS, Directory: $DIR"
 
 if [[ ! -z "$1" ]]; then
     echo "Using custom SDL library path: $1"
     sdl_library_file_path="$1"
-elif [[ "$uname_str" == "linux" ]]; then
-    sdl_library_file_path="$script_dir/lib/libSDL2-2.0.so"
-elif [[ "$uname_str" == "darwin" ]]; then
-    sdl_library_file_path="$script_dir/lib/libSDL2-2.0.dylib"
+elif [[ "$OS" == "Microsoft" ]]; then
+    sdl_library_file_path="$DIR/lib/SDL2.dll"
+elif [[ "$OS" == "Linux" ]]; then
+    sdl_library_file_path="$DIR/lib/libSDL2-2.0.so"
+elif [[ "$OS" == "Apple" ]]; then
+    sdl_library_file_path="$DIR/lib/libSDL2-2.0.dylib"
 fi
 
 if [[ ! -z "$2" ]]; then
     echo "Using custom SDL include path: $2"
     sdl_include_directory_path="$2"
-elif [ ! -d "$script_dir/SDL" ]; then
-    git clone https://github.com/libsdl-org/SDL $script_dir/SDL
-    sdl_include_directory_path="$script_dir/SDL/include"
+elif [ ! -d "$DIR/SDL" ]; then
+    echo "Using SDL include path from clone"
+    git clone https://github.com/libsdl-org/SDL $DIR/SDL
+    sdl_include_directory_path="$DIR/SDL/include"
 else
-    cd $script_dir/SDL
+    echo "Using SDL include path from clone"
+    cd $DIR/SDL
     git pull
-    cd $script_dir
-    sdl_include_directory_path="$script_dir/SDL/include"
+    cd $DIR
+    sdl_include_directory_path="$DIR/SDL/include"
 fi
 
 if [ ! -f "$sdl_library_file_path" ]; then
-    build_dir_sdl="$script_dir/cmake-build-release-sdl"
-    cmake -S $script_dir/SDL -B $build_dir_sdl
+    build_dir_sdl="$DIR/cmake-build-release-sdl"
+    cmake $CMAKE_TOOLCHAIN_ARGS -S $DIR/SDL -B $build_dir_sdl -DSDL_STATIC=OFF
     cmake --build $build_dir_sdl --config Release
-    if [[ "$uname_str" == "linux" ]]; then
+    if [[ "$OS" == "Linux" ]]; then
         shared_object_path="$(readlink -f $build_dir_sdl/libSDL2-2.0.so)"
         mv "$shared_object_path" "$sdl_library_file_path"
-    elif [[ "$uname_str" == "darwin" ]]; then
+    elif [[ "$OS" == "Apple" ]]; then
         shared_object_path="$build_dir_sdl/libSDL2-2.0.dylib"
+        mv "$shared_object_path" "$sdl_library_file_path"
+    elif [[ "$OS" == "Microsoft" ]]; then
+        shared_object_path="$build_dir_sdl/SDL2.dll"
         mv "$shared_object_path" "$sdl_library_file_path"
     fi
     rm -r $build_dir_sdl
-    rm -r $script_dir/SDL
+    rm -r $DIR/SDL
 fi
 
-build_dir="$script_dir/cmake-build-release-fna3d"
-lib_dir="$script_dir/lib"
+build_dir="$DIR/cmake-build-release-fna3d"
+lib_dir="$DIR/lib"
 
-cmake -S $script_dir/ext/FNA3D -B $build_dir -DSDL2_INCLUDE_DIRS="$sdl_include_directory_path" -DSDL2_LIBRARIES="$sdl_library_file_path"
+cmake $CMAKE_TOOLCHAIN_ARGS -S $DIR/ext/FNA3D -B $build_dir -DSDL2_INCLUDE_DIRS="$sdl_include_directory_path" -DSDL2_LIBRARIES="$sdl_library_file_path"
 cmake --build $build_dir --config Release
 mkdir -p $lib_dir
 
-filepath="$(perl -MCwd -e 'print Cwd::abs_path shift' $build_dir/libFNA3D.dylib)"
-if [[ "$uname_str" == "linux" ]]; then
+if [[ "$OS" == "Linux" ]]; then
+    filepath="$(perl -MCwd -e 'print Cwd::abs_path shift' $build_dir/libFNA3D.so)"
     mv "$filepath" "$lib_dir/libFNA3D.so"
-elif [[ "$uname_str" == "darwin" ]]; then
+elif [[ "$OS" == "Apple" ]]; then
+    filepath="$(perl -MCwd -e 'print Cwd::abs_path shift' $build_dir/libFNA3D.dylib)"
     mv "$filepath" "$lib_dir/libFNA3D.dylib"
     lc_rpath="$(dirname $sdl_library_file_path)"
     install_name_tool -delete_rpath "$lc_rpath" "$lib_dir/libFNA3D.dylib"
+elif [[ "$OS" == "Microsoft" ]]; then
+    filepath="$(perl -MCwd -e 'print Cwd::abs_path shift' $build_dir/FNA3D.dll)"
+    mv "$filepath" "$lib_dir/FNA3D.dll"
 fi
 
 rm -r $build_dir
