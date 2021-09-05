5b39717c9d6f78b1eaee793b2e92487e16356fd0
Lucas Girouard-Stranks <lustranks@gmail.com>
2021-09-05 16:52:10 -0400

    Update library.sh

--- library.sh
+++ library.sh
@@ -1,56 +1,64 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+LIB_DIR="$DIR/lib"
+mkdir -p $LIB_DIR
+
+if [[ ! -z "$1" ]]; then
+    BUILD_PLATFORM="$1"
+fi
+if [[ ! -z "$2" ]]; then
+    SDL_LIBRARY_FILE_PATH="$2"
+fi
+if [[ ! -z "$3" ]]; then
+    SDL_INCLUDE_DIRECTORY_PATH="$3"
+fi
+
 echo "Started '$0'"
 
-function set_default_build_platform() {
+function set_target_build_platform_host() {
     uname_str="$(uname -a)"
     case "${uname_str}" in
-        *Microsoft*)    BUILD_PLATFORM="Microsoft";;
-        *microsoft*)    BUILD_PLATFORM="Microsoft";;
-        Linux*)         BUILD_PLATFORM="Linux";;
-        Darwin*)        BUILD_PLATFORM="Apple";;
-        CYGWIN*)        BUILD_PLATFORM="Linux";;
-        MINGW*)         BUILD_PLATFORM="Microsoft";;
-        *Msys)          BUILD_PLATFORM="Microsoft";;
-        *)              BUILD_PLATFORM="UNKNOWN:${uname_str}"
+        *Microsoft*)    TARGET_BUILD_PLATFORM="microsoft";;
+        *microsoft*)    TARGET_BUILD_PLATFORM="microsoft";;
+        Linux*)         TARGET_BUILD_PLATFORM="linux";;
+        Darwin*)        TARGET_BUILD_PLATFORM="apple";;
+        CYGWIN*)        TARGET_BUILD_PLATFORM="linux";;
+        MINGW*)         TARGET_BUILD_PLATFORM="microsoft";;
+        *Msys)          TARGET_BUILD_PLATFORM="microsoft";;
+        *)              TARGET_BUILD_PLATFORM="UNKNOWN:${uname_str}"
     esac
 }
 
-function get_build_platform {
-    if [[ ! -z "$1" ]]; then
-        if [[ $1 == "default" ]]; then
-            set_default_build_platform
-            echo "Build platform: '$BUILD_PLATFORM' (host default)"
+function set_target_build_platform {
+    if [[ ! -z "$TARGET_BUILD_PLATFORM" ]]; then
+        if [[ $TARGET_BUILD_PLATFORM == "default" ]]; then
+            set_target_build_platform_host
+            echo "Build platform: '$TARGET_BUILD_PLATFORM' (host default)"
         else
-            BUILD_PLATFORM="$1"
-            if [[ "$BUILD_PLATFORM" == "Microsoft" || "$BUILD_PLATFORM" == "Linux" || "$BUILD_PLATFORM" == "Apple" ]]; then
-                echo "Build platform: '$BUILD_PLATFORM' (cross-compile override)"
+            if [[ "$TARGET_BUILD_PLATFORM" == "microsoft" || "$TARGET_BUILD_PLATFORM" == "linux" || "$TARGET_BUILD_PLATFORM" == "apple" ]]; then
+                echo "Build platform: '$TARGET_BUILD_PLATFORM' (cross-compile override)"
             else
-                echo "Unknown '$BUILD_PLATFORM' passed as first argument. Use 'default' to use the host build platform."
+                echo "Unknown '$TARGET_BUILD_PLATFORM' passed as first argument. Use 'default' to use the host build platform."
                 exit 1
             fi
         fi
     else
-        set_default_build_platform
-        echo "Build platform: '$BUILD_PLATFORM' (host default)"
+        set_target_build_platform_host
+        echo "Build platform: '$TARGET_BUILD_PLATFORM' (host default)"
     fi
 }
 
-get_build_platform
-if [[ "$BUILD_PLATFORM" == "Microsoft" ]]; then
+set_target_build_platform
+if [[ "$TARGET_BUILD_PLATFORM" == "microsoft" ]]; then
     CMAKE_TOOLCHAIN_ARGS="-DCMAKE_TOOLCHAIN_FILE=$DIR/mingw-w64-x86_64.cmake"
-elif [[ "$BUILD_PLATFORM" == "Linux" ]]; then
+elif [[ "$TARGET_BUILD_PLATFORM" == "linux" ]]; then
     CMAKE_TOOLCHAIN_ARGS=""
-elif [[ "$BUILD_PLATFORM" == "Apple" ]]; then
+elif [[ "$TARGET_BUILD_PLATFORM" == "apple" ]]; then
     CMAKE_TOOLCHAIN_ARGS=""
 else
-    echo "Unknown: $BUILD_PLATFORM"
+    echo "Unknown: $TARGET_BUILD_PLATFORM"
     exit 1
 fi
-
-echo "Started building native libraries..."
-LIB_DIR="$DIR/lib"
-mkdir -p $LIB_DIR
 
 function exit_if_last_command_failed() {
     error=$?
@@ -63,31 +71,29 @@ function exit_if_last_command_failed() {
 function build_sdl() {
     echo "Building SDL..."
 
-    if [[ ! -z "$1" ]]; then
-        SDL_LIBRARY_FILE_PATH="$1"
+    if [[ ! -z "$SDL_LIBRARY_FILE_PATH" ]]; then
         SDL_LIBRARY_FILE_NAME="$(dirname SDL_LIBRARY_FILE_PATH)"
         if [ ! -f "$SDL_LIBRARY_FILE_PATH" ]; then
-            echo "Custom SDL library path '$1' does not exist!"
+            echo "Custom SDL library path '$SDL_LIBRARY_FILE_PATH' does not exist!"
         else
-            echo "Using custom SDL library path: $1"
+            echo "Using custom SDL library path: $SDL_LIBRARY_FILE_PATH"
         fi
-    elif [[ "$BUILD_PLATFORM" == "Microsoft" ]]; then
+    elif [[ "$TARGET_BUILD_PLATFORM" == "microsoft" ]]; then
         SDL_LIBRARY_FILE_NAME="SDL2.dll"
         SDL_LIBRARY_FILE_PATH="$LIB_DIR/$SDL_LIBRARY_FILE_NAME"
-    elif [[ "$BUILD_PLATFORM" == "Linux" ]]; then
+    elif [[ "$TARGET_BUILD_PLATFORM" == "linux" ]]; then
         SDL_LIBRARY_FILE_NAME="libSDL2-2.0.so"
         SDL_LIBRARY_FILE_PATH="$LIB_DIR/$SDL_LIBRARY_FILE_NAME"
-    elif [[ "$BUILD_PLATFORM" == "Apple" ]]; then
+    elif [[ "$TARGET_BUILD_PLATFORM" == "apple" ]]; then
         SDL_LIBRARY_FILE_NAME="libSDL2-2.0.dylib"
         SDL_LIBRARY_FILE_PATH="$LIB_DIR/$SDL_LIBRARY_FILE_NAME"
     fi
 
-    if [[ ! -z "$2" ]]; then
-        SDL_INCLUDE_DIRECTORY_PATH="$2"
+    if [[ ! -z "$SDL_INCLUDE_DIRECTORY_PATH" ]]; then
         if [ ! -d "$SDL_INCLUDE_DIRECTORY_PATH" ]; then
-            echo "Custom SDL include path '$2' does not exist!"
+            echo "Custom SDL include path '$SDL_INCLUDE_DIRECTORY_PATH' does not exist!"
         else
-            echo "Using custom SDL include path: $2"
+            echo "Using custom SDL include path: $SDL_INCLUDE_DIRECTORY_PATH"
         fi
     elif [ ! -d "$DIR/SDL" ]; then
         git clone https://github.com/libsdl-org/SDL $DIR/SDL
@@ -106,11 +112,11 @@ function build_sdl() {
         cmake $CMAKE_TOOLCHAIN_ARGS -S $DIR/SDL -B $SDL_BUILD_DIR -DSDL_STATIC=OFF -DSDL_TEST=OFF
         cmake --build $SDL_BUILD_DIR --config Release
 
-        if [[ "$BUILD_PLATFORM" == "Linux" ]]; then
+        if [[ "$TARGET_BUILD_PLATFORM" == "linux" ]]; then
             SDL_LIBRARY_FILE_PATH_BUILD="$(readlink -f $SDL_BUILD_DIR/$SDL_LIBRARY_FILE_NAME)"
-        elif [[ "$BUILD_PLATFORM" == "Apple" ]]; then
+        elif [[ "$TARGET_BUILD_PLATFORM" == "apple" ]]; then
             SDL_LIBRARY_FILE_PATH_BUILD="$SDL_BUILD_DIR/$SDL_LIBRARY_FILE_NAME"
-        elif [[ "$BUILD_PLATFORM" == "Microsoft" ]]; then
+        elif [[ "$TARGET_BUILD_PLATFORM" == "microsoft" ]]; then
             SDL_LIBRARY_FILE_PATH_BUILD="$SDL_BUILD_DIR/$SDL_LIBRARY_FILE_NAME"
         fi
 
@@ -136,13 +142,13 @@ function build_fna3d() {
     cmake $CMAKE_TOOLCHAIN_ARGS -S $DIR/ext/FNA3D -B $FNA3D_BUILD_DIR -DSDL2_INCLUDE_DIRS="$SDL_INCLUDE_DIRECTORY_PATH" -DSDL2_LIBRARIES="$SDL_LIBRARY_FILE_PATH"
     cmake --build $FNA3D_BUILD_DIR --config Release
 
-    if [[ "$BUILD_PLATFORM" == "Linux" ]]; then
+    if [[ "$TARGET_BUILD_PLATFORM" == "linux" ]]; then
         FNA3D_LIBRARY_FILENAME="libFNA3D.so"
         FNA3D_LIBRARY_FILE_PATH_BUILD="$(readlink -f $FNA3D_BUILD_DIR/$FNA3D_LIBRARY_FILENAME)"
-    elif [[ "$BUILD_PLATFORM" == "Apple" ]]; then
+    elif [[ "$TARGET_BUILD_PLATFORM" == "apple" ]]; then
         FNA3D_LIBRARY_FILENAME="libFNA3D.dylib"
         FNA3D_LIBRARY_FILE_PATH_BUILD="$FNA3D_BUILD_DIR/$FNA3D_LIBRARY_FILENAME"
-    elif [[ "$BUILD_PLATFORM" == "Microsoft" ]]; then
+    elif [[ "$TARGET_BUILD_PLATFORM" == "microsoft" ]]; then
         FNA3D_LIBRARY_FILENAME="FNA3D.dll"
         FNA3D_LIBRARY_FILE_PATH_BUILD="$FNA3D_BUILD_DIR/$FNA3D_LIBRARY_FILENAME"
     fi
@@ -164,7 +170,6 @@ function build_fna3d() {
 
 build_sdl
 build_fna3d
-echo "Finished building native libraries!"
 ls -d "$LIB_DIR"/*
 
-echo "Ended '$0'"
+echo "Finished '$0'!"
