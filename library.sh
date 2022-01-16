c0104f645524a23112d797e84b9c2b61af73694c
Lucas Girouard-Stranks <lustranks@gmail.com>
2022-01-16 16:13:46 -0500

    Update `library.sh` + FNA3D

--- library.sh
+++ library.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo "Started building native libraries... Directory: $DIR"
 LIB_DIR="$DIR/lib"
 mkdir -p $LIB_DIR
-
 echo "Started '$0' $1 $2 $3 $4"
 
 if [[ ! -z "$1" ]]; then
@@ -22,24 +22,38 @@ if [[ ! -z "$4" ]]; then
 fi
 
 function set_target_build_os {
-    if [[ -z "$TARGET_BUILD_OS" || $TARGET_BUILD_OS == "default" ]]; then
+    if [[ -z "$TARGET_BUILD_OS" || $TARGET_BUILD_OS == "host" ]]; then
         uname_str="$(uname -a)"
         case "${uname_str}" in
-            *Microsoft*)    TARGET_BUILD_OS="microsoft";;
-            *microsoft*)    TARGET_BUILD_OS="microsoft";;
+            *Microsoft*)    TARGET_BUILD_OS="windows";;
+            *microsoft*)    TARGET_BUILD_OS="windows";;
             Linux*)         TARGET_BUILD_OS="linux";;
-            Darwin*)        TARGET_BUILD_OS="apple";;
+            Darwin*)        TARGET_BUILD_OS="macos";;
             CYGWIN*)        TARGET_BUILD_OS="linux";;
-            MINGW*)         TARGET_BUILD_OS="microsoft";;
-            *Msys)          TARGET_BUILD_OS="microsoft";;
+            MINGW*)         TARGET_BUILD_OS="windows";;
+            *Msys)          TARGET_BUILD_OS="windows";;
             *)              TARGET_BUILD_OS="UNKNOWN:${uname_str}"
         esac
-        echo "Target build operating system: '$TARGET_BUILD_OS' (default)"
+
+        if [[
+            "$TARGET_BUILD_OS" != "windows" &&
+            "$TARGET_BUILD_OS" != "macos" &&
+            "$TARGET_BUILD_OS" != "linux"
+        ]]; then
+            echo "Unknown target build operating system: $TARGET_BUILD_OS"
+            exit 1
+        fi
+
+        echo "Target build operating system: '$TARGET_BUILD_OS' (host)"
     else
-        if [[ "$TARGET_BUILD_OS" == "microsoft" || "$TARGET_BUILD_OS" == "linux" || "$TARGET_BUILD_OS" == "apple" ]]; then
+        if [[
+            "$TARGET_BUILD_OS" == "windows" ||
+            "$TARGET_BUILD_OS" == "macos" ||
+            "$TARGET_BUILD_OS" == "linux"
+            ]]; then
             echo "Target build operating system: '$TARGET_BUILD_OS' (override)"
         else
-            echo "Unknown '$TARGET_BUILD_OS' passed as first argument. Use 'default' to use the host build platform or use either: 'microsoft', 'linux', 'apple'."
+            echo "Unknown '$TARGET_BUILD_OS' passed as first argument. Use 'host' to use the host build platform or use either: 'windows', 'macos', 'linux'."
             exit 1
         fi
     fi
@@ -47,7 +61,12 @@ function set_target_build_os {
 
 function set_target_build_arch {
     if [[ -z "$TARGET_BUILD_ARCH" || $TARGET_BUILD_ARCH == "default" ]]; then
-        TARGET_BUILD_ARCH="$(uname -m)"
+        if [[ "$TARGET_BUILD_OS" == "macos" ]]; then
+            TARGET_BUILD_ARCH="x86_64;arm64"
+        else
+            TARGET_BUILD_ARCH="$(uname -m)"
+        fi
+
         echo "Target build CPU architecture: '$TARGET_BUILD_ARCH' (default)"
     else
         if [[ "$TARGET_BUILD_ARCH" == "x86_64" || "$TARGET_BUILD_ARCH" == "arm64" ]]; then
@@ -57,33 +76,13 @@ function set_target_build_arch {
             exit 1
         fi
     fi
+    if [[ "$TARGET_BUILD_OS" == "macos" ]]; then
+        CMAKE_ARCH_ARGS="-DCMAKE_OSX_ARCHITECTURES=$TARGET_BUILD_ARCH"
+    fi
 }
 
 set_target_build_os
 set_target_build_arch
-
-if [[ "$TARGET_BUILD_OS" != "microsoft" && "$TARGET_BUILD_OS" != "apple" && "$TARGET_BUILD_OS" != "linux" ]]; then
-    echo "Unknown target build operating system: $TARGET_BUILD_OS"
-    exit 1
-fi
-
-if [[ "$TARGET_BUILD_ARCH" == "x86_64" ]]; then
-    if [[ "$TARGET_BUILD_OS" == "microsoft" ]]; then
-        CMAKE_TOOLCHAIN_ARGS="-DCMAKE_TOOLCHAIN_FILE=$DIR/mingw-w64-x86_64.cmake"
-    elif [[ "$TARGET_BUILD_OS" == "apple" ]]; then
-        CMAKE_ARCH_ARGS="-DCMAKE_OSX_ARCHITECTURES=x86_64"
-    fi
-elif [[ "$TARGET_BUILD_ARCH" == "arm64" ]]; then
-    if [[ "$TARGET_BUILD_OS" == "microsoft" ]]; then
-        echo "ARM64 not yet supported for Windows."
-        exit 1
-    elif [[ "$TARGET_BUILD_OS" == "apple" ]]; then
-        CMAKE_ARCH_ARGS="-DCMAKE_OSX_ARCHITECTURES=arm64"
-    fi
-else
-    echo "Unknown target build CPU architecture: $TARGET_BUILD_ARCH"
-    exit 1
-fi
 
 function exit_if_last_command_failed() {
     error=$?
@@ -92,6 +91,7 @@ function exit_if_last_command_failed() {
         exit $error
     fi
 }
+
 
 function build_sdl() {
     echo "Building SDL..."
@@ -103,8 +103,8 @@ function build_sdl() {
         else
             echo "Using custom SDL library path: $SDL_LIBRARY_FILE_PATH"
         fi
-    elif [[ "$TARGET_BUILD_OS" == "microsoft" ]]; then
-        SDL_LIBRARY_FILE_NAME="SDL2.dll"
+    elif [[ "$TARGET_BUILD_OS" == "windows" ]]; then
+        SDL_LIBRARY_FILE_NAME="SDL2.lib"
         SDL_LIBRARY_FILE_PATH="$LIB_DIR/$SDL_LIBRARY_FILE_NAME"
     elif [[ "$TARGET_BUILD_OS" == "linux" ]]; then
         SDL_LIBRARY_FILE_NAME="libSDL2-2.0.so"
@@ -134,15 +134,22 @@ function build_sdl() {
 
     if [ ! -f "$SDL_LIBRARY_FILE_PATH" ]; then
         SDL_BUILD_DIR="$DIR/cmake-build-release-sdl"
-        cmake $CMAKE_TOOLCHAIN_ARGS -S $DIR/SDL -B $SDL_BUILD_DIR $CMAKE_ARCH_ARGS -DSDL_STATIC=OFF -DSDL_TEST=OFF
+        rm -rf SDL_BUILD_DIR
+
+        cmake -S $DIR/SDL -B $SDL_BUILD_DIR $CMAKE_ARCH_ARGS \
+            `#Change output directories` \
+            -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY=$SDL_BUILD_DIR -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=$SDL_BUILD_DIR -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=$SDL_BUILD_DIR -DCMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE=$SDL_BUILD_DIR \
+            `# project specific` \
+            -DSDL_STATIC=OFF -DSDL_TEST=OFF -DSDL_LEAN_AND_MEAN=1
+        
         cmake --build $SDL_BUILD_DIR --config Release
 
         if [[ "$TARGET_BUILD_OS" == "linux" ]]; then
             SDL_LIBRARY_FILE_PATH_BUILD="$(readlink -f $SDL_BUILD_DIR/$SDL_LIBRARY_FILE_NAME)"
-        elif [[ "$TARGET_BUILD_OS" == "apple" ]]; then
+        elif [[ "$TARGET_BUILD_OS" == "macos" ]]; then
             SDL_LIBRARY_FILE_PATH_BUILD="$SDL_BUILD_DIR/$SDL_LIBRARY_FILE_NAME"
-        elif [[ "$TARGET_BUILD_OS" == "microsoft" ]]; then
-            SDL_LIBRARY_FILE_PATH_BUILD="$SDL_BUILD_DIR/$SDL_LIBRARY_FILE_NAME"
+        elif [[ "$TARGET_BUILD_OS" == "windows" ]]; then
+            SDL_LIBRARY_FILE_PATH_BUILD="$SDL_BUILD_DIR/Release/$SDL_LIBRARY_FILE_NAME"
         fi
 
         if [[ ! -f "$SDL_LIBRARY_FILE_PATH_BUILD" ]]; then
@@ -164,16 +171,22 @@ function build_sdl() {
 function build_fna3d() {
     echo "Building FNA3D..."
     FNA3D_BUILD_DIR="$DIR/cmake-build-release-fna3d"
-    cmake $CMAKE_TOOLCHAIN_ARGS -S $DIR/ext/FNA3D -B $FNA3D_BUILD_DIR $CMAKE_ARCH_ARGS -DSDL2_INCLUDE_DIRS="$SDL_INCLUDE_DIRECTORY_PATH" -DSDL2_LIBRARIES="$SDL_LIBRARY_FILE_PATH"
+    rm -rf FNA3D_BUILD_DIR
+
+    cmake -S $DIR/ext/FNA3D -B $FNA3D_BUILD_DIR $CMAKE_ARCH_ARGS \
+        `#Change output directories` \
+        -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY=$FNA3D_BUILD_DIR -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=$FNA3D_BUILD_DIR -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=$FNA3D_BUILD_DIR -DCMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE=$FNA3D_BUILD_DIR \
+        `# project specific` \
+        -DSDL2_INCLUDE_DIRS="$SDL_INCLUDE_DIRECTORY_PATH" -DSDL2_LIBRARIES="$SDL_LIBRARY_FILE_PATH"
     cmake --build $FNA3D_BUILD_DIR --config Release
 
     if [[ "$TARGET_BUILD_OS" == "linux" ]]; then
         FNA3D_LIBRARY_FILENAME="libFNA3D.so"
         FNA3D_LIBRARY_FILE_PATH_BUILD="$(readlink -f $FNA3D_BUILD_DIR/$FNA3D_LIBRARY_FILENAME)"
-    elif [[ "$TARGET_BUILD_OS" == "apple" ]]; then
+    elif [[ "$TARGET_BUILD_OS" == "macos" ]]; then
         FNA3D_LIBRARY_FILENAME="libFNA3D.dylib"
         FNA3D_LIBRARY_FILE_PATH_BUILD="$(perl -MCwd -e 'print Cwd::abs_path shift' $FNA3D_BUILD_DIR/$FNA3D_LIBRARY_FILENAME)"
-    elif [[ "$TARGET_BUILD_OS" == "microsoft" ]]; then
+    elif [[ "$TARGET_BUILD_OS" == "windows" ]]; then
         FNA3D_LIBRARY_FILENAME="FNA3D.dll"
         FNA3D_LIBRARY_FILE_PATH_BUILD="$FNA3D_BUILD_DIR/$FNA3D_LIBRARY_FILENAME"
     fi
@@ -184,7 +197,7 @@ function build_fna3d() {
         exit 1
     fi
 
-    if [[ "$TARGET_BUILD_OS" == "apple" ]]; then
+    if [[ "$TARGET_BUILD_OS" == "macos" ]]; then
         install_name_tool -delete_rpath "$(dirname $SDL_LIBRARY_FILE_PATH)" $FNA3D_LIBRARY_FILE_PATH_BUILD
         install_name_tool -change @rpath/libSDL2-2.0.dylib @executable_path/libSDL2.dylib $FNA3D_LIBRARY_FILE_PATH_BUILD
     fi
@@ -192,8 +205,8 @@ function build_fna3d() {
     mv "$FNA3D_LIBRARY_FILE_PATH_BUILD" "$FNA3D_LIBRARY_FILE_PATH"
     exit_if_last_command_failed
     echo "Copied '$FNA3D_LIBRARY_FILE_PATH_BUILD' to '$FNA3D_LIBRARY_FILE_PATH'"
-
-    rm -rf $FNA3D_BUILD_DIR
+`
+    rm -rf $FNA3D_BUILD_DIR`
     exit_if_last_command_failed
     echo "Building FNA3D finished!"
 }
